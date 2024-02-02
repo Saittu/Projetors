17fcd890d8b35991eee3075df46d12743e9773bb
Jparlim <157638906+Jparlim@users.noreply.github.com>
2024-02-01 22:47:16 -0300

    Projeto-RS

---
+++ app.js
@@ -0,0 +1,15 @@
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle("light")
+
+
+    const img = document.querySelector("#profile.img")
+
+if (html.classList.contains('light')) {
+
+    img.setAttribut('src', './image/avatar-light.svg')
+} else {
+    img.setAttribute('src', './image/avatar.svg')
+    }
+}
+
